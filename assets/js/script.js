aaab82e67232ff2166cff77cc66cc19c518fb0c1
Thulio Paiva <thuliopv360@gmail.com>
2022-05-20 22:32:20 -0300

    adicao das imagens e correcao do bug no script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,49 +1,39 @@
-const paletas = await response.json();
 const baseUrl = "http://localhost:3000/paletas";
 
 async function findAllPaletas() {
-    const response = await fetch(`${baseUrl}/find-paletas`);
+    const response = await fetch(`${baseUrl}/todas-paletas`);
 
     const paletas = await response.json();
-};
-paletas.forEach((paleta) => {
-            document.getElementById("paletaList").insertAdjacentHTML(
-                    "beforeend",
-                    `<div class="PaletaListaItem">
-      <div>
-          <div class="PaletaListaItem__sabor">${paleta.sabor}</div>
-          <div class="PaletaListaItem__preco">R$ ${paleta.preco.toFixed(
-            2
-          )}</div>
-          <div class="PaletaListaItem__descricao">${paleta.descricao}</div>
-        </div>
-          <img class="PaletaListaItem__foto" src=${
-            paleta.foto
-          } alt=${`Paleta de ${paleta.sabor}`} />
-      </div>`
-  );
-});
 
-const findPaletaById = async () => {
-    const id = document.getElementById("idPaleta").value;
+    paletas.forEach(function(paleta) {
+                document.querySelector("#paletaList").insertAdjacentHTML("beforeend", `<div class="PaletaListaItem">
+          <div>
+            <div class="PaletaListaItem__sabor">${paleta.sabor}</div>
+            <div class="PaletaListaItem__preco">R$ ${paleta.preco}</div>
+            <div class="PaletaListaItem__descricao">${paleta.descricao}</div>
+          </div>
+          <img class="PaletaListaItem__foto" src="./${paleta.foto}" alt=${`Paleta de ${paleta.sabor}`} />
+        </div>`
+        );
+})}
+
+async function findPaletaById(){
+  const id = document.querySelector("#idPaleta").value;
   
-    const response = await fetch(`${baseUrl}/find-paleta/${id}`);
+  const response = await fetch(`${baseUrl}/paleta/${id}`);
+  const paleta = await response.json();
+  const paletaEscolhidaDiv = document.querySelector("#paletaEscolhida");
   
-    const paleta = await response.json();
-  
-    const paletaEscolhidaDiv = document.getElementById("paletaEscolhida");
-  
-    paletaEscolhidaDiv.innerHTML = `<div class="PaletaCardItem">
+  paletaEscolhidaDiv.innerHTML = `
+    <div class="PaletaCardItem">
       <div>
         <div class="PaletaCardItem__sabor">${paleta.sabor}</div>
-        <div class="PaletaCardItem__preco">R$ ${paleta.preco.toFixed(2)}</div>
+        <div class="PaletaCardItem__preco">R$ ${paleta.preco}</div>
         <div class="PaletaCardItem__descricao">${paleta.descricao}</div>
       </div>
-        <img class="PaletaCardItem__foto" src=${
-          paleta.foto
-        } alt=${`Paleta de ${paleta.sabor}`} />
+      <img class="PaletaCardItem__foto" src="./${paleta.foto}" alt=${`Paleta de ${paleta.sabor}`} />
     </div>`;
-  };
+  
+}
 
 findAllPaletas();
-findPaletaById();
